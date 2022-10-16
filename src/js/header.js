50d5a3cb35873ca1a353e5a05ee5ad086fdb0c55
darkeden48 <dzalex72@list.ru>
2022-10-16 20:54:16 +0300

    complete

--- src/js/header.js
+++ src/js/header.js
@@ -3,14 +3,28 @@ const headerLibrary = document.querySelector('.library');
 const linkHome = document.querySelectorAll('#home');
 const linkLibrary = document.querySelectorAll('#library');
 const body = document.querySelector('body');
-console.log(body)
+const main = document.querySelector('.main');
+const mainLibrary = document.querySelector('.main-library');
+
 function onLinkHome(event){
-    headerLibrary.classList.add('invisable')
-    header.classList.remove('invisable')
+    headerLibrary.classList.add('invisable');
+    header.classList.remove('invisable');
+    mainLibrary.classList.add('invisable');
+    main.classList.remove('invisable');
+    // body.insertBefore(main,body.firstChild);
+    // body.insertBefore(header,main); 
+    // headerLibrary.remove();
+    // mainLibrary.remove();
 }
 function onLinkLibrary(event){
     headerLibrary.classList.remove('invisable');
-    header.classList.add('invisable');    
+    header.classList.add('invisable'); 
+    mainLibrary.classList.remove('invisable');
+    main.classList.add('invisable'); 
+    // body.insertBefore(mainLibrary,body.firstChild);
+    // body.insertBefore(headerLibrary,mainLibrary);  
+    // header.remove();
+    // main.remove();
 }
 
 linkHome[0].addEventListener('click', onLinkHome);
