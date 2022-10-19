e175be898d54884d0fa4547039fc3bd4dff3e6ef
darkeden48 <dzalex72@list.ru>
2022-10-19 23:07:34 +0300

    comp

--- src/js/loadTrend.js
+++ src/js/loadTrend.js
@@ -2,21 +2,31 @@ import ApiServiceTMDB from '../apiService/ApiService';
 import loadTrend from '../views/loadFilms.hbs';
 import getTotalPages from './pagination';
 
-
 const galleryList = document.querySelector('.collection');
+const release_dateTMP = document.getElementsByClassName('film-card__release');
+const genres_nameTMP = document.getElementsByClassName('film-card__genres');
+const movie_votesTMP = document.getElementsByClassName('film-card__vote');
+const movie_votesCountTMP = document.getElementsByClassName('film-card');
 
+   
 // Movie Render
 function appendImgMarkup(image) {
     galleryList.insertAdjacentHTML('beforeend', loadTrend(image));
-
+    
     ApiServiceTMDB.fetchTrendFilms().then(function() {
-        const release_dateTMP = document.getElementsByClassName('film-card__release');
-        const genres_nameTMP = document.getElementsByClassName('film-card__genres');
-        const movie_votesTMP = document.getElementsByClassName('film-card__vote');
-
         // Hide votes statistics
+        // for (let i = 0; i < movie_votesTMP.length; i++) {
+        //     movie_votesTMP[i].style.display = 'none';
+        // }
+        // Slice votes statistics
         for (let i = 0; i < movie_votesTMP.length; i++) {
-            movie_votesTMP[i].style.display = 'none';
+            let stringVote = String (movie_votesTMP[i].innerHTML.slice(0, 3));
+            if (stringVote % 1 != 0) {
+                movie_votesTMP[i].innerHTML = Number(stringVote);
+            }
+            else {
+                movie_votesTMP[i].innerHTML = Number(stringVote)+'.0';
+            }
         }
         // Setting release date into template
         for (let i = 0; i < release_dateTMP.length; i++) {
@@ -32,12 +42,12 @@ function appendImgMarkup(image) {
                 const fetched_names = genre_results.map(genre => genre.name);
 
                 separatedIds.forEach(function(element, index) {
-                    for (let u = 0; u < fetched_ids.length; u++) {
-                        if(element == fetched_ids[u]) {
-                            this[index] =  ' ' + fetched_names[u];
-                        }
-                        genres_nameTMP[i].innerHTML = separatedIds;
-                    }
+            for (let u = 0; u < fetched_ids.length; u++) {
+                if(element == fetched_ids[u]) {
+                    this[index] =  ' ' + fetched_names[u];
+                }
+                    genres_nameTMP[i].innerHTML = separatedIds;
+            }
                 }, separatedIds);
             }
         });
@@ -50,6 +60,7 @@ function onLoadTrend(page) {
     ApiServiceTMDB.fetchTrendFilms(page).then(appendImgMarkup);
     getTotalPages();
 };
-onLoadTrend(ApiServiceTMDB.page);
+
+        onLoadTrend(ApiServiceTMDB.page);
 
 export default onLoadTrend;
