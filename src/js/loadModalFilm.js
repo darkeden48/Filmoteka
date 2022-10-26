566f14d40e24bdbf7e93932959362d8ad2721587
darkeden48 <dzalex72@list.ru>
2022-10-26 20:59:10 +0300

    save

--- src/js/loadModalFilm.js
+++ src/js/loadModalFilm.js
@@ -8,7 +8,7 @@ const backdrop = document.querySelector('.backdrop');
 const modalContent = document.querySelector('.modal__content');
 const modal = document.querySelector('.film_modal');
 const btnClose = document.querySelector('.close-svg');
-console.log(btnClose)
+
 function appendImgMarkup(image) {
     modalContent.insertAdjacentHTML('beforeend', loadModal(image));
     modalCard();
@@ -19,10 +19,8 @@ function onLoadModal(event) {
     if(event.target.classList.contains('film-card__img')){
         backdrop.classList.remove('is-hidden');
         ApiServiceTMDB.fetchFilmById(event.target.dataset.id).then(appendImgMarkup);
-        console.dir(event.target.dataset.id)
     }
     else {
-        console.log('lol')
         return
     }
 };
@@ -31,17 +29,13 @@ function closeModal(e){
     modalContent.innerHTML = '';
 }
 function onCrossClick(evt) {
-    console.dir(evt.target)
     if (evt.target.contains(btnClose)||evt.target.contains(btnClose.children[0])) {
-        console.log(btnClose)
         closeModal();
     }
 }
 
 function onModalClick(evt) {
-    console.log(evt.target)
     if (backdrop === evt.target ) {
-        console.log(evt.target)
         closeModal();
     }
 }
