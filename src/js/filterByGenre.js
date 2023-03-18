1448e7514c81cb3044dd45dcbcb22932b6aad04d
darkeden48 <dzalex72@list.ru>
2023-03-18 15:33:26 +0200

    save

--- src/js/filterByGenre.js
+++ src/js/filterByGenre.js
@@ -2,6 +2,8 @@ import ApiServiceTMDB from '../apiService/ApiService';
 import loadTrend from '../views/loadFilms.hbs';
 import filmCard from './film-card';
 
+const body = document.querySelector('body');
+const genreInput = document.querySelector('.input-genre');
 const genreList = document.querySelector('.genres-list');
 const galleryList = document.querySelector('.collection');
 
@@ -15,9 +17,9 @@ ApiServiceTMDB.fetchGenres().then(data =>
 );
 
 function appendImgMarkup(image) {
-  console.log(loadTrend(image));
   galleryList.insertAdjacentHTML('beforeend', loadTrend(image));
   ApiServiceTMDB.fetchFilmsByGenre().then(filmCard());
+  genreList.style.display = 'none';
   //   if (!galleryList.hasChildNodes()) {
   //     document.querySelector('#search_error').innerHTML =
   //       'Search result not successful. Enter the correct movie!';
@@ -28,8 +30,24 @@ function appendImgMarkup(image) {
 
 const genrePicker = e => {
   galleryList.innerHTML = '';
-  console.log(e.target.id);
+  genreInput.value = e.target.innerHTML;
   ApiServiceTMDB.fetchFilmsByGenre(e.target.id).then(appendImgMarkup);
 };
 
+function showGenres(e) {
+  if (e.target === genreInput) {
+    genreList.style.display = 'block';
+  }
+}
+
+function closeGenreList(e) {
+  //   if (e.target !== genreInput) {
+  //     genreList.style.display = 'none';
+  //   }
+  if (e.target !== genreList && e.target !== genreInput)
+    genreList.style.display = 'none';
+}
+
+body.addEventListener('click', closeGenreList);
+genreInput.addEventListener('click', showGenres);
 genreList.addEventListener('click', genrePicker);
