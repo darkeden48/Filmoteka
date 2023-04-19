da353ba1536c4a1e2b94973803ae2bfacb4eda0c
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-04-19 22:57:26 +0300

    Added Google authentication + added firebase deploy

--- firebase.config.js
+++ firebase.config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, OAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,7 +18,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// Firebase storage
 const db = getFirestore(app);
 
-export { db, auth }
+// Authentication methods
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const microsoftProvider = new OAuthProvider('microsoft.com');
+
+export { db, auth, googleProvider, microsoftProvider, githubProvider }
--- src/js/authentication.js
+++ src/js/authentication.js
@@ -1,16 +1,23 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '../../firebase.config';
+import {
+    createUserWithEmailAndPassword, signInWithEmailAndPassword,
+    signOut, signInWithRedirect, getRedirectResult,
+    GoogleAuthProvider, OAuthProvider, GithubAuthProvider
+} from 'firebase/auth';
+import {
+    auth, googleProvider,
+    microsoftProvider, githubProvider
+} from '../../firebase.config';
 
 // Registration
 const registerForm = document.querySelector('#registerForm');
 registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
     // Input Values
-    let username = document.querySelector('#registerUsername').value;
-    let email = document.querySelector('#registerEmail').value;
-    let password = document.querySelector('#registerPassword').value;
-    let repeatPassword = document.querySelector('#registerRepeatPassword').value;
-    let error = document.querySelector('#registerError');
+    const username = document.querySelector('#registerUsername').value;
+    const email = document.querySelector('#registerEmail').value;
+    const password = document.querySelector('#registerPassword').value;
+    const repeatPassword = document.querySelector('#registerRepeatPassword').value;
+    const error = document.querySelector('#registerError');
 
     if (password === repeatPassword) {
         createUserWithEmailAndPassword(auth, email, password)
@@ -18,7 +25,7 @@ registerForm.addEventListener('submit', (e) => {
                 signOut(auth);
                 // Redirect to login form
                 document.querySelector('#login').click();
-                
+
                 // Clear data
                 registerForm.reset();
                 error.removeAttribute('style');
@@ -33,22 +40,20 @@ registerForm.addEventListener('submit', (e) => {
     error.innerHTML = 'Your password and confirmation password do not match.';
 });
 
-// Sign in
+// Sign in with email and password
 const loginForm = document.querySelector('#loginForm');
+const error = document.querySelector('#loginError');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     // Input Values
-    let email = document.querySelector('#loginEmail').value;
-    let password = document.querySelector('#loginPassword').value;
-    let error = document.querySelector('#loginError');
+    const email = document.querySelector('#loginEmail').value;
+    const password = document.querySelector('#loginPassword').value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then(userCredential => {
             // Signed in
             document.querySelector('#home').click();
-            error.style.removeAttribute('style');
-            console.log(userCredential.user);
-            
+
             // Clear data
             registerForm.reset();
             error.removeAttribute('style');
@@ -57,4 +62,46 @@ loginForm.addEventListener('submit', (e) => {
             error.style.display = 'block';
             error.innerHTML = err.message;
         });
-})
+});
+
+// Sign in with Google
+const signInWithGoogle = document.querySelector('#googleLogin');
+signInWithGoogle.addEventListener('click', (e) => {
+    e.preventDefault();
+    signInWithProvider(googleProvider, GoogleAuthProvider);
+});
+
+// Sign in with GitHub
+const signInWithGitHub = document.querySelector('#githubLogin');
+signInWithGitHub.addEventListener('click', (e) => {
+    e.preventDefault();
+    signInWithProvider(githubProvider, GithubAuthProvider);
+});
+
+// Sign in with Microsoft
+const signInWithMicrosoft = document.querySelector('#microsoftLogin');
+signInWithMicrosoft.addEventListener('click', (e) => {
+    e.preventDefault();
+    signInWithProvider(microsoftProvider, OAuthProvider);
+});
+
+async function signInWithProvider(provider, providerCredential) {
+    await signInWithRedirect(auth, provider);
+    getRedirectResult(auth)
+        .then(result => {
+            // Getting the Access Token, so user can use it to access the Google API
+            const credential = providerCredential.credentialFromResult(result);
+            const token = credential.accessToken;
+            // Signed in user
+            const user = result.user;
+
+            // Redirect user after login & clear the form
+            console.log(token);
+            document.querySelector('#home').click();
+            error.removeAttribute('style');
+            loginForm.reset();
+        }).catch(err => {
+            error.style.display = 'block';
+            error.innerHTML = err.message;
+        });
+}
